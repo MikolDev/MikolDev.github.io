3ac5e8146f92a98b5215ad861290b97f24a0250a
Mikołaj <mikol.cicho@gmail.com>
2024-12-11 00:04:59 +0100

    education slider

--- src/app/components/education-section/education-section.component.ts
+++ src/app/components/education-section/education-section.component.ts
@@ -1,13 +1,48 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { Component } from '@angular/core';
+import { SchoolInfo } from '../../types/school-info';
 import { ScrollTriggerDirective } from '../../directives/scroll-trigger.directive';
 
 @Component({
   selector: 'app-education-section',
   standalone: true,
-  imports: [ ScrollTriggerDirective],
+  imports: [ ScrollTriggerDirective, HttpClientModule],
   templateUrl: './education-section.component.html',
   styleUrl: './education-section.component.scss'
 })
 export class EducationSectionComponent {
+  schools: SchoolInfo[] = [];
+  currentSchoolIndex = 0;
+  currentSchool: SchoolInfo | null = null;
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<SchoolInfo[]>('assets/data/education.json').subscribe((data) => {
+      this.schools = data;
+      this.currentSchool = this.schools[this.currentSchoolIndex];
+    });
+    
+  }
+
+  prev() {
+    if (this.currentSchoolIndex > 0) {
+      this.currentSchoolIndex--;
+    } else {
+      this.currentSchoolIndex = this.schools.length - 1;
+    }
+    this.currentSchool = this.schools[this.currentSchoolIndex];
+    console.log(this.currentSchool);
+  }
+
+  next() {
+    if (this.currentSchoolIndex < this.schools.length - 1) {
+      this.currentSchoolIndex++;
+    } else {
+      this.currentSchoolIndex = 0;
+    }
+    this.currentSchool = this.schools[this.currentSchoolIndex];
+    console.log(this.currentSchool);
+  }
 }
