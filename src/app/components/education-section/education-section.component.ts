87b39dfc9147da50fbf2c509753d97adb02700fc
Mikołaj <mikol.cicho@gmail.com>
2024-12-11 19:47:38 +0100

    slider animations

--- src/app/components/education-section/education-section.component.ts
+++ src/app/components/education-section/education-section.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SchoolInfo } from '../../types/school-info';
 import { ScrollTriggerDirective } from '../../directives/scroll-trigger.directive';
@@ -7,7 +8,7 @@ import { ScrollTriggerDirective } from '../../directives/scroll-trigger.directiv
 @Component({
   selector: 'app-education-section',
   standalone: true,
-  imports: [ ScrollTriggerDirective, HttpClientModule],
+  imports: [ ScrollTriggerDirective, HttpClientModule, CommonModule],
   templateUrl: './education-section.component.html',
   styleUrl: './education-section.component.scss'
 })
@@ -15,6 +16,8 @@ export class EducationSectionComponent {
   schools: SchoolInfo[] = [];
   currentSchoolIndex = 0;
   currentSchool: SchoolInfo | null = null;
+  fadingOut = false;
+  fadingIn = false;
 
   constructor(private http: HttpClient) { }
 
@@ -22,27 +25,40 @@ export class EducationSectionComponent {
     this.http.get<SchoolInfo[]>('assets/data/education.json').subscribe((data) => {
       this.schools = data;
       this.currentSchool = this.schools[this.currentSchoolIndex];
-    });
-    
+    }); 
   }
 
-  prev() {
+  async runAnimation() {
+    this.fadingOut = true;
+    await this.delay(500);
+    this.fadingOut = false;
+    this.fadingIn = true;
+  }
+
+  async prev() {
+    if (this.fadingOut) return;
     if (this.currentSchoolIndex > 0) {
       this.currentSchoolIndex--;
     } else {
       this.currentSchoolIndex = this.schools.length - 1;
     }
+    await this.runAnimation();
     this.currentSchool = this.schools[this.currentSchoolIndex];
-    console.log(this.currentSchool);
   }
 
-  next() {
+  async next() {
+    if (this.fadingOut) return;
     if (this.currentSchoolIndex < this.schools.length - 1) {
       this.currentSchoolIndex++;
     } else {
       this.currentSchoolIndex = 0;
     }
+    await this.runAnimation();
     this.currentSchool = this.schools[this.currentSchoolIndex];
-    console.log(this.currentSchool);
+  }
+
+  // delaying for animations
+  delay(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 }
